6c579b6376abb275ef3c9512e978af4b03aa74f3
Nick Kusters <github@nickkusters.com>
2024-05-13 09:55:46 +0200

    hacked in EOS Mainnet support

--- Program.cs
+++ Program.cs
@@ -15,12 +15,17 @@ namespace ValidateWaxPK
         const string PrimaryKeyPrefix = "pk:";
         const string TestNetFlag = "testnet";
         const string TestNetContractName = "nkcss55nkcss";
+        const string EosMainNetContractName = "greymassnoop";
+        const string EosMainNetActionName = "noop";
         const int ExpectedPrivateKeyLength = 51;
 
         const string WaxMainNetChainId = "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4";
         const string WaxMainNetApiUrl = "https://wax.greymass.com";
         const string WaxTestNetChainId = "f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12";
         const string WaxTestNetApiUrl = "https://testnet.waxsweden.org";
+
+        const string EosMainNetChainId = "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906";
+        const string EosMainNetApiUrl = "http://eos.eosusa.io";
 
         static async Task<(string result, string tx, bool error)> Execute(EosApi api, params EosSharp.Core.Api.v1.Action[] actions)
         {
@@ -62,7 +67,7 @@ namespace ValidateWaxPK
         }
         static async Task Main(string[] args)
         {
-            bool UseTestNet = false;
+            bool UseTestNet = false, useWax = false;
             Abi2CSharp.Model.eosio.Name wallet = null;
             string pk = null;
             if ((args?.Length ?? 0) > 1)
@@ -107,8 +112,8 @@ namespace ValidateWaxPK
                 Environment.Exit(1);
             }
 
-            string ChainId = UseTestNet ? WaxTestNetChainId : WaxMainNetChainId;
-            string ApiUrl = UseTestNet ? WaxTestNetApiUrl : WaxMainNetApiUrl;
+            string ChainId = useWax ? (UseTestNet ? WaxTestNetChainId : WaxMainNetChainId) : EosMainNetChainId;
+            string ApiUrl = useWax ? (UseTestNet ? WaxTestNetApiUrl : WaxMainNetApiUrl) : EosMainNetApiUrl;
 
             var api = new EosApi(new EosConfigurator
             {
@@ -116,8 +121,23 @@ namespace ValidateWaxPK
                 HttpEndpoint = ApiUrl,
                 ChainId = ChainId
             }, new HttpHandler());
-            var req = Abi2CSharp.Contracts.boost_wax.Requests.noop.CreateAction(wallet, new Abi2CSharp.Contracts.boost_wax.Types.noop { });
-            if (UseTestNet) req.account = TestNetContractName;
+            EosSharp.Core.Api.v1.Action req;
+            if (useWax)
+            {
+                req = Abi2CSharp.Contracts.boost_wax.Requests.noop.CreateAction(wallet, new Abi2CSharp.Contracts.boost_wax.Types.noop { });
+                if (UseTestNet) req.account = TestNetContractName;
+            }
+            else
+            {
+                req = new EosSharp.Core.Api.v1.Action {
+                    account = EosMainNetContractName,
+                    name = EosMainNetActionName,
+                    authorization = new List<PermissionLevel> {
+                        new PermissionLevel { actor = wallet, permission = "active" }
+                    },
+                    data = new { }
+                };
+            }
             var result = await Execute(api, req);
             if (result.error)
             {
